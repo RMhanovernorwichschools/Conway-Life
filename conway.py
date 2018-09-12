b07bf15aa1979524969c54dc1fae71f0397e45ea
RMhanovernorwichschools <37371520+RMhanovernorwichschools@users.noreply.github.com>
2018-09-12 08:01:06 -0400

    Updated from Brython Server: 9/12/2018 8:01:05 AM

--- conway.py
+++ conway.py
@@ -1,8 +1,9 @@
 """
 conway.py
-Author: <your name here>
+Author: Rachel Matthew
 Credit: <list sources used, if any>
 Assignment:
 Write and submit a program that plays Conway's Game of Life, per 
 https://github.com/HHS-IntroProgramming/Conway-Life
 """
+from ggame import RectangleAsset, Color, LineStyle
