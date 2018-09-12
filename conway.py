aaf6aa48c743ef68b244c691e1194581403a37cb
RMhanovernorwichschools <37371520+RMhanovernorwichschools@users.noreply.github.com>
2018-09-12 08:04:35 -0400

    Updated from Brython Server: 9/12/2018 8:04:34 AM

--- conway.py
+++ conway.py
@@ -6,4 +6,11 @@ Assignment:
 Write and submit a program that plays Conway's Game of Life, per 
 https://github.com/HHS-IntroProgramming/Conway-Life
 """
-from ggame import RectangleAsset, Color, LineStyle
+from ggame import RectangleAsset, Color, LineStyle, App
+
+class Conway(App):
+    def __init__(self):
+        super().__init__()
+        white=Color(0xfff0ff, 1.0)
+        black=Color(0x000000, 1.0)
+        nl=LineStyle(0, black)
