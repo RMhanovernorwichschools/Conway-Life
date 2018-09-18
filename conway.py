313b3ea0e753165341e86ac15e4c99f3e76bfab9
RMhanovernorwichschools <37371520+RMhanovernorwichschools@users.noreply.github.com>
2018-09-18 07:27:53 -0400

    Updated from Brython Server: 9/18/2018 7:27:52 AM

--- conway.py
+++ conway.py
@@ -35,7 +35,7 @@ class Conway(App):
             
     def initiate(self, event):
         self.start()
-        self.refresh()
+        print('a one')
         a=time.time()
         while True:
             if time.time()>a+1.5:
